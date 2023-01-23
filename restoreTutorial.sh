64c6d6ac7564f1ddaa25ec97c053761b87f221da
Gregor / Serverlaptop <mentor@kidslab.de>
2023-01-23 11:53:49 +0100

    Schweinfurth und Kösching vorbereitet Zusätzliche Mods installiert und abhängigkeiten aufgelöst

--- restoreTutorial.sh
+++ restoreTutorial.sh
@@ -2,6 +2,6 @@
 cp main-config/tutorial.conf main-config/minetest.conf
 docker-compose -f tutorial.yaml stop
 rm debug.txt
-rm -R /opt/minetest-zfn-docker/worlds/Tutorial
-cp -R /opt/minetest-zfn-docker/Tutorial/worlds/Tutorial  /opt/minetest-zfn-docker/worlds
+rm -R worlds/Tutorial
+cp -R minetest-Tutorial/worlds/Tutorial  worlds/
 # docker-compose -f tutorial.yaml up
--- start.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-read -p "Welche Welt willst Du Starten? (01, 02, 03 etc.) : " world
-echo $world
-EXPORT WORLDNAME=$world
-cp main-config/workshop.conf main-config/minetest.conf
-docker-compose -f workshop-old.yaml stop
-rm debug.txt
-docker-compose -f workshop.yaml up
