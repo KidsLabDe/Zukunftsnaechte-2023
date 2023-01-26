87507eb6a49f95ada603f1af95a68b3769eea07e
mentor <mentor@minetest-laptop>
2023-01-26 17:24:07 +0100

    Skripte überarbeitet. Desktop-Dateien erzeugt.

--- restoreTutorial.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-cp main-config/tutorial.conf main-config/minetest.conf
-docker-compose -f tutorial.yaml stop
-rm debug.txt
-rm -R worlds/Tutorial
-cp -R minetest-Tutorial/worlds/Tutorial  worlds/
-# docker-compose -f tutorial.yaml up
--- startWorkshop.sh
+++ startWorkshop.sh
@@ -1,10 +1,12 @@
-#/bin/bash
+#!/bin/bash
+
+cd /opt/minetest
 read -p "Welche Welt willst Du Starten? (01, 02, 03 etc.) : " world
 echo $world
 export WORLDNAME=$world
 cp main-config/workshop.conf main-config/minetest.conf
 rm mods/world2minetest/map.dat
 cp worlds/$WORLDNAME*/world2minetest/map.dat mods/world2minetest/
-#docker-compose -f workshop-old.yaml stop
+#docker-compose -f workshop-old.yaml sto
 rm debug.txt
 docker-compose -f workshop-old.yaml up
